163785a2c241019197bc4c442afca796e7263313
Dmitry <dkamzolov@yandex.ru>
2022-04-27 00:04:05 +0400

    .zero_().add_() -> set_() closure(backward) -> closure().backward()

--- OPTAMI/first_order/similar_triangles.py
+++ OPTAMI/first_order/similar_triangles.py
@@ -98,7 +98,7 @@ class SimilarTriangles(Optimizer):
                     alpha = state['alpha']
 
                     u.sub_(p.grad, alpha=a)
-                    p.set_(x, alpha=alpha).add_(u, alpha=1 - alpha)
+                    p.set_(x.mul(alpha)).add_(u, alpha=1 - alpha)
 
                     state['u'] = u
 
