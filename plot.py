00c629975e5037e260acfa4e5767abc96722db49
dmivilensky <pasechnyuk2004@gmail.com>
2022-06-02 21:30:55 +0400

    plots

--- plot.py
+++ plot.py
@@ -12,7 +12,7 @@ markers = ["o", "v", "d", "s", "^", "<", ">", "+"]
 DATASET = "a9a"
 F_START = 0.6931471805599453
 F_STAR  = 0.32501597692415846
-WITH_TIME = True
+WITH_TIME = False
 
 LOG_PATH = f"logs_{DATASET}"
 for i, file in enumerate(sorted(os.listdir(LOG_PATH))):
@@ -46,4 +46,4 @@ axs[1].set_ylim(1e-8, 1e-1)
 
 plt.tight_layout()
 postfix = "time" if WITH_TIME else "iters"
-plt.savefig(f"figure_{postfix}.pdf")
+plt.savefig(f"figure_{postfix}.jpg", dpi=400)
