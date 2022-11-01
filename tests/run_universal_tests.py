70da820a3357b9f0ea35a8943426d31d8032ccf1
Dmitry <dkamzolov@yandex.ru>
2022-11-02 00:49:22 +0400

    Tests

--- tests/run_universal_tests.py
+++ tests/run_universal_tests.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 
+import torch
 from sklearn.datasets import load_svmlight_file
 from sklearn.preprocessing import normalize
 import warnings
-from OPTAMI.utils.fit import func_fit
-import OPTAMI
-import torch
 import sys
+sys.path.append("./")
+
+import OPTAMI
+from OPTAMI.utils.fit import func_fit
+
 
 if not sys.warnoptions:
     warnings.simplefilter("ignore")
