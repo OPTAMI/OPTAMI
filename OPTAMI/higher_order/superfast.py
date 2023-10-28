f1b4037a5504f87ea56b7ec13020bb3fc0767de7
Kamzolov Dmitry <kamzolov.opt@gmail.com>
2023-10-28 19:34:56 +0400

    Superfast method simplified

--- OPTAMI/higher_order/superfast.py
+++ OPTAMI/higher_order/superfast.py
@@ -3,7 +3,6 @@ import math
 import warnings
 from torch.optim.optimizer import Optimizer
 import OPTAMI
-from OPTAMI.utils import tuple_to_vec
 
 
 class Superfast(Optimizer):
@@ -32,6 +31,7 @@ class Superfast(Optimizer):
         max_iters (int): number of the inner iterations of the subsolver to solve the inner problem (default: None)
     """
     MONOTONE = False
+    SKIP_TEST_LOGREG = False
 
     def __init__(self, params, L: float = 1., order: int = 3, a_step: float = None,
                  TensorStepMethod: Optimizer = None, tensor_step_kwargs: dict = None,
@@ -41,23 +41,50 @@ class Superfast(Optimizer):
             raise ValueError(f"Invalid learning rate: L = {L}")
 
         super().__init__(params, dict(L=L))
+
+        self.verbose = verbose
+        self.testing = testing
         if len(self.param_groups) != 1:
             raise ValueError("Superfast doesn't support per-parameter options "
                              "(parameter groups)")
+        group = self.param_groups[0]
+        params = group['params']
+        p = next(iter(params))
+        state_common = self.state[p]
+        state_common['k'] = 0
+
         self.order = order
-        self.TensorStepMethod = TensorStepMethod
-        self.subsolver = subsolver
-        self.subsolver_args = subsolver_args
-        self.max_iters = max_iters
-        self.tensor_step_kwargs = tensor_step_kwargs
-        self.verbose = verbose
-        self.testing = testing
+
         if a_step is None:
             a_step = (2 * order - 1) / (2 * (order + 1) * (2 * order + 1)) \
                      * math.factorial(order - 1) * 2 * (1 / (2 * order)) ** order
         self.a_step = a_step
-        self.tensor_step_method = None
 
+        # Step initialization
+        self.tensor_step_method = None
+        if TensorStepMethod is None:
+            if self.order == 3:
+                self.tensor_step_method = OPTAMI.BasicTensorMethod(
+                    params, L=L, subsolver=subsolver, verbose=self.verbose,
+                    subsolver_args=subsolver_args, max_iters=max_iters, testing=self.testing)
+            elif self.order == 2:
+                self.tensor_step_method = OPTAMI.CubicRegularizedNewton(
+                    params, L=L, subsolver=subsolver, verbose=self.verbose,
+                    subsolver_args=subsolver_args, max_iters=max_iters, testing=self.testing)
+            else:  # order = 1
+                self.tensor_step_method = OPTAMI.GradientDescent(params, L=L, testing=self.testing)
+        else:
+            if not hasattr(TensorStepMethod, 'MONOTONE') or not TensorStepMethod.MONOTONE:
+                warnings.warn("`TensorStepMethod` should be monotone!")
+            self.tensor_step_method = TensorStepMethod(params, **tensor_step_kwargs)
+
+        # Initialization of intermediate points
+        for p in params:
+            state = self.state[p]
+            state['x0'] = p.detach().clone()
+            state['x'] = state['x0'].clone()
+            state['v'] = state['x0'].clone()
+            state['df_sum'] = torch.zeros_like(p)
 
     def step(self, closure):
         """Performs a single optimization step.
@@ -65,80 +92,51 @@ class Superfast(Optimizer):
             closure (callable): a closure that reevaluates the model and returns the loss.
         """
         closure = torch.enable_grad()(closure)
-        assert len(self.param_groups) == 1
 
+        assert len(self.param_groups) == 1
         group = self.param_groups[0]
         params = group['params']
-        L = group['L']
-
         p = next(iter(params))
         state_common = self.state[p]
 
-        if 'k' not in state_common:
-            state_common['k'] = 0
-
         k = state_common['k']
-        if self.tensor_step_method is None:
-            if self.TensorStepMethod is None:
-                if self.order == 3:
-                    self.tensor_step_method = OPTAMI.BasicTensorMethod(
-                        params, L=L, subsolver=self.subsolver, verbose=self.verbose,
-                        subsolver_args=self.subsolver_args, max_iters=self.max_iters, testing=self.testing)
-                elif self.order == 2:
-                    self.tensor_step_method = OPTAMI.CubicRegularizedNewton(
-                        params, L=L, subsolver=self.subsolver, verbose=self.verbose,
-                        subsolver_args=self.subsolver_args, max_iters=self.max_iters, testing=self.testing)
-                else:  # order = 1
-                    self.tensor_step_method = OPTAMI.GradientDescent(params, L=L, testing=self.testing)
-            else:
-                if not hasattr(self.TensorStepMethod, 'MONOTONE') or not self.TensorStepMethod.MONOTONE:
-                    warnings.warn("`TensorStepMethod` should be monotone!")
-                self.tensor_step_method = self.TensorStepMethod(params, **self.tensor_step_kwargs)
+
+        L = group['L']
 
         alpha = (1. - 1 / (k + 1)) ** (self.order + 1)
 
-        for p in params:
-            state = self.state[p]
-
-            if ('v' not in state) or ('x' not in state):
-                state['x0'] = p.detach().clone()
-                state['x'] = state['x0'].clone()
-                state['v'] = state['x0'].clone()
-                state['df_sum'] = torch.zeros_like(p)
-
-            with torch.no_grad():
-                v = state['v']
-                p.mul_(alpha).add_(v, alpha=1 - alpha)
+        with torch.no_grad():
+            for p in params:
+                state = self.state[p]
+                p.mul_(alpha).add_(state['v'], alpha=1 - alpha)
 
         self.tensor_step_method.step(closure)
         self.zero_grad()
 
         closure().backward()
 
-        a = self.a_step * ((k + 1) ** (self.order + 1) - k ** (self.order + 1)) / L
-        for p in params:
-            state = self.state[p]
-            with torch.no_grad():
+        with torch.no_grad():
+            a = self.a_step * ((k + 1) ** (self.order + 1) - k ** (self.order + 1)) / L
+            for p in params:
+                state = self.state[p]
                 state['x'].zero_().add_(p)
                 state['df_sum'].add_(p.grad, alpha=a)
 
-        if self.order == 1:
-            scalier = 1.
-        else:
-            norm_squared = 0.
+            if self.order == 1:
+                scalier = 1.
+            else:
+                norm_squared = 0.
+                for p in params:
+                    state = self.state[p]
+                    norm_squared += state['df_sum'].square().sum()
+
+                power = (1. - self.order) / (2. * self.order)
+                scalier = norm_squared ** power
+
             for p in params:
                 state = self.state[p]
-                with torch.no_grad():
-                    norm_squared += tuple_to_vec.tuple_norm_square(state['df_sum'])
-
-            power = (1. - self.order) / (2. * self.order)
-            scalier = norm_squared ** power
-
-        for p in params:
-            state = self.state[p]
-            with torch.no_grad():
                 state['v'].zero_().add_(state['x0']).sub_(state['df_sum'], alpha=scalier)
 
-        state_common['k'] += 1
+            state_common['k'] += 1
 
         return None
