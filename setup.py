0971a48e492f4f5b40f1d114737a8b64085a91c1
Kamzolov Dmitry <kamzolov.opt@gmail.com>
2024-10-23 21:54:33 +0400

    Major Update

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+        name="OPTAMI",
+        version="0.0.1",
+        author="Dmitry Kamzolov",
+        author_email="kamzolov.opt@gmail.com",
+        description="Second-Order PyTorch Optimizers",
+        long_description=open("README.md").read(),
+        long_description_content_type="text/markdown",
+        url="https://github.com/OPTAMI/OPTAMI",
+        packages=find_packages(),
+        python_requires= '>=3.6',
+        install_requires=[],
+        keywords=['python', 'pytorch', 'optimization', 'optimizer'],
+        classifiers= [
+            "Programming Language :: Python :: 3",
+            "Operating System :: OS Independent",
+        ]
+)
